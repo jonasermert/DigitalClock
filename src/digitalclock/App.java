8979c47ff4d62168a8d5fe8ffed4fc99c3bfc2dc
Jonas <ermert.jonas@yahoo.de>
2021-11-15 00:42:44 +0100

    Added imports

--- src/digitalclock/App.java
+++ src/digitalclock/App.java
@@ -1,10 +1,16 @@
 package digitalclock;
 
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 public class App {
 
     public static void main (String[] args) {
 
-        
+
     }
 
 
